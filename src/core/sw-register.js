d36290148f81635ef7fb02f51ae0de1d50761ac1
Roa089 <35975748+Roa089@users.noreply.github.com>
2026-02-04 18:22:45 +0100

    Add service worker registration function

--- src/core/sw-register.js
+++ src/core/sw-register.js
@@ -1,1 +1,8 @@
+// src/core/sw-register.js
+export function registerServiceWorker(path = "./sw.js") {
+  if (!("serviceWorker" in navigator)) return;
 
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register(path).catch(() => {});
+  });
+}
